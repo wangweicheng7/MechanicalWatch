30302e28e8d46ed1b562bf9349a3fedc8911307d
weicheng.wang <weicheng.wang@shangnaxue.net>
2023-04-25 17:16:04 +0800

    Initial Commit

---
+++ MechanicalWatch/MechanicalWatchView.m
@@ -0,0 +1,51 @@
+//
+//  MechanicalWatchView.m
+//  MechanicalWatch
+//
+//  Created by weicheng wang on 2023/4/25.
+//
+
+#import "MechanicalWatchView.h"
+
+@implementation MechanicalWatchView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
